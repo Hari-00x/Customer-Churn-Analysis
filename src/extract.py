a03c767b34b350724fa855c662fa778797da59ae
Hari-360 <hmanday89@gmail.com>
2026-08-24 15:42:05 +0530

    Analysis of data

--- src/extract.py
+++ src/extract.py
@@ -15,14 +15,31 @@ df_support=pd.read_sql_query(
     "select * from db_support",connection
 )
 
-print("\nCustomer Table")
+print("\nCustomer Table and Info")
 print(df_customer.head())
+print(df_customer.shape)
+print(df_customer.columns)
+print(df_customer.dtypes)
+print(df_customer.isnull().sum())
+print("Duplicate rows:",df_customer.duplicated().sum())
 
-print("\nSubscription Table")
+print("\nSubscription Table and Info")
 print(df_subscription.head())
+print(df_subscription.head())
+print(df_subscription.shape)
+print(df_subscription.columns)
+print(df_subscription.dtypes)
+print(df_subscription.isnull().sum())
+print("Duplicate rows:",df_subscription.duplicated().sum())
 
-print("\nSupport Table")
+print("\nSupport Table and Info")
 print(df_support.head())
+print(df_support.head())
+print(df_support.shape)
+print(df_support.columns)
+print(df_support.dtypes)
+print(df_support.isnull().sum())
+print("Duplicate rows:",df_support.duplicated().sum())
 
 
 connection.close()
