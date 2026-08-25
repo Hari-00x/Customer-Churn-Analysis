dc0b58bd79f349697619a0acac4e0a17315cefe2
Hari-360 <hmanday89@gmail.com>
2026-08-25 15:11:02 +0530

    Added as function to reuse the result

--- src/clean.py
+++ src/clean.py
@@ -1,44 +1,49 @@
 import sqlite3
 import pandas as pd
 
-connection=sqlite3.connect("data/database/customer_churn.db")
 
-df_customer=pd.read_sql_query(
-    "select * from db_customer",
-    connection
-)
+def load_and_clean_data():
+    
+    connection=sqlite3.connect("data/database/customer_churn.db")
 
-df_customer["dob"]=pd.to_datetime(df_customer["dob"])
+    df_customer=pd.read_sql_query(
+        "select * from db_customer",
+        connection
+    )
 
-df_customer=df_customer.drop(columns=["pincode","interests"])
+    df_customer["dob"]=pd.to_datetime(df_customer["dob"])
 
-df_subscription=pd.read_sql_query(
-    "select * from db_subscription",
-    connection
-)
+    df_customer=df_customer.drop(columns=["pincode","interests"])
 
-df_subscription["subscription_start_date"]=pd.to_datetime(df_subscription["subscription_start_date"])
+    df_subscription=pd.read_sql_query(
+        "select * from db_subscription",
+        connection
+    )
 
-df_subscription["renewal_date"]=pd.to_datetime(df_subscription["renewal_date"])
+    df_subscription["subscription_start_date"]=pd.to_datetime(df_subscription["subscription_start_date"])
 
-df_subscription["cancellation_date"]=pd.to_datetime(df_subscription["cancellation_date"])\
+    df_subscription["renewal_date"]=pd.to_datetime(df_subscription["renewal_date"])
 
-df_subscription["subscription_type"] = df_subscription["subscription_type"].replace(
-    "Refferal",
-    "Referral"
-) 
+    df_subscription["cancellation_date"]=pd.to_datetime(df_subscription["cancellation_date"])\
+
+    df_subscription["subscription_type"] = df_subscription["subscription_type"].replace(
+        "Refferal",
+        "Referral"
+    ) 
 
 
-df_support=pd.read_sql_query(
-    "select * from db_support",
-    connection
-)
+    df_support=pd.read_sql_query(
+        "select * from db_support",
+        connection
+    )
 
-df_support["complaint_date"]=pd.to_datetime(df_support["complaint_date"])
+    df_support["complaint_date"]=pd.to_datetime(df_support["complaint_date"])
 
-df_support=df_support.drop(columns="col_1")
+    df_support=df_support.drop(columns="col_1")
 
-connection.close()
+    connection.close()
+
+    return df_customer,df_subscription,df_support
 
 
 
